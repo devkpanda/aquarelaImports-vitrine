f1d5eac913e7a6e78a71352a1a3eec0d1896d658
Ghriphos <luisfernando._alves@hotmail.com>
2023-10-03 20:39:08 -0300

    fix: user/add route

--- app/controllers/CtrlUsers.php
+++ app/controllers/CtrlUsers.php
@@ -5,8 +5,10 @@ use models\User;
 $url = parse_url($_SERVER['REQUEST_URI']);
 $uriPath = $url['path'];
 
-if ($uriPath == 'user/add') {
-    if ($_SERVER['REQUEST_METHOD'] == 'post'){
+echo $uriPath;
+
+if ($uriPath == '/user/add') {
+    if (strtolower($_SERVER['REQUEST_METHOD']) == 'post'){
         $json = file_get_contents('php://input');
 
         if ($json === false){
@@ -27,10 +29,11 @@ if ($uriPath == 'user/add') {
                 $recovery_phrase = $data['recovery_phrase'];
 
                 $user = new User(0, $idNivelUsuario, $name, $email, $password, 0, $active_code, $recovery_phrase);
+
                 if ($user->save()) {
-                    echo json_encode(array('status_code' => '0'));
+                    echo json_encode(array('message' => '0'));
                 } else {
-                    echo json_encode(array('status_code' => '1'));
+                    echo json_encode(array('message' => '1'));
                 }
             }   
         }
