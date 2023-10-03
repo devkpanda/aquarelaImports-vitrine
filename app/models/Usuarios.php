d5b9b559b6c62a4d6a8cc51caaf522fa33fd7b74
Ghriphos <luisfernando._alves@hotmail.com>
2023-10-03 19:02:34 -0300

    feat: user class

--- app/models/Usuarios.php
+++ app/models/Usuarios.php
@@ -7,41 +7,44 @@ use core\database\DBQuery;
 class Usuarios {
     
     private $id;
+    private $idNivelUsuario;
     private $name;
-    private $phone_number;
     private $email;
     private $password;
     private $active;
-    private $id_user_level;
+    private $active_code;
+    private $recovery_phrase;
     
     private $dbquery;
     
-    function __construct( $id, $name, $phone_number, $email, $password, $active, $id_user_level){
+    function __construct($id, $idNivelUsuario, $name, $email, $password, $active, $active_code, $recovery_phrase){
         
         $tableName  = "user";
-        $fieldsName = "id, name, phone_number, email, password, active, id_user_level";
+        $fieldsName = "id, idNivelUsuario, name, email, password, active, active_code, recovery_phrase";
         $fieldKey   = "id";
         $this->dbquery = new DBQuery($tableName, $fieldsName, $fieldKey);
         
         $this->setId( $id );
+        $this->setIdNivelUsuario( $idNivelUsuario );
         $this->setName( $name );
-        $this->setPhoneNumber( $phone_number );
         $this->setEmail( $email );
         $this->setPassword( $password );
         $this->setActive( $active );
-        $this->setIdUserLevel( $id_user_level );
+        $this->setActive_code( $active_code );
+        $this->setRecovery_phrase( $recovery_phrase );
     }
     
     public function toArray(){
         return array(
             $this->getId(),
+            $this->getIdNivelUsuario(),
             $this->getName(),
-            $this->getPhoneNumber(),
             $this->getEmail(),
             $this->getPassword(),
             $this->getActive(),
-            $this->getIdUserLevel()
-            );
+            $this->getActive_code(),
+            $this->getRecovery_phrase()
+        );
     }
     
     public function toString(){
@@ -77,15 +80,16 @@ class Usuarios {
         
         if ($rSet) {
             foreach ($rSet as $linha) {
-                $id             = $linha['id'];
-                $name           = $linha['name'];
-                $phone_number   = $linha['phone_number'];
-                $email          = $linha['email'];
-                $password       = $linha['senha'];
-                $active         = $linha['active'];
-                $id_user_level  = $linha['id_user_level'];
+                $id                 = $linha['id'];
+                $idNivelUsuario     = $linha['idNivelUsuario'];
+                $name               = $linha['name'];
+                $email              = $linha['email'];
+                $password           = $linha['senha'];
+                $active             = $linha['active'];
+                $active_code        = $linha['active_code'];
+                $recovery_phrase    = $linha['recovery_phrase'];
                 
-                $usuarios[] = new Usuarios($id, $name, $phone_number, $email, $password, $active, $id_user_level);                }
+                $usuarios[] = new Usuarios($id, $idNivelUsuario, $name, $email, $password, $active, $active_code, $recovery_phrase);                }
         } else {
             $usuarios[] = array();
             echo  "{'msg':'Nenhum usuário encontrado.\n'}";
@@ -99,62 +103,167 @@ class Usuarios {
         }
     }
     
-    public function setId ($id ){
-        $this->id = $id;
-    }
     
-    public function getId(){
-        return( $this->id );
-    }
-    
-    public function setName( $name ){
-        $this->name = $name;
-    }
-    
-    public function getName(){
-        return( $this->name );
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
     }
 
-    public function setPhoneNumber( $phone_number ){
-        $this->phone_number = $phone_number;
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
     }
-    
-    public function getPhoneNumber(){
-        return( $this->phone_number );
+
+    /**
+     * Get the value of idNivelUsuario
+     */ 
+    public function getIdNivelUsuario()
+    {
+        return $this->idNivelUsuario;
     }
-    
-    public function setEmail( $email ){
+
+    /**
+     * Set the value of idNivelUsuario
+     *
+     * @return  self
+     */ 
+    public function setIdNivelUsuario($idNivelUsuario)
+    {
+        $this->idNivelUsuario = $idNivelUsuario;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setEmail($email)
+    {
         $this->email = $email;
+
+        return $this;
     }
-    
-    public function getEmail(){
-        return( $this->email );
+
+    /**
+     * Get the value of password
+     */ 
+    public function getPassword()
+    {
+        return $this->password;
     }
-    
-    public function setPassword( $password ){
+
+    /**
+     * Set the value of password
+     *
+     * @return  self
+     */ 
+    public function setPassword($password)
+    {
         $this->password = $password;
+
+        return $this;
     }
-    
-    public function getPassword(){
-        return( $this->password );
+
+    /**
+     * Get the value of active
+     */ 
+    public function getActive()
+    {
+        return $this->active;
     }
-    
-    public function setActive( $active ){
+
+    /**
+     * Set the value of active
+     *
+     * @return  self
+     */ 
+    public function setActive($active)
+    {
         $this->active = $active;
+
+        return $this;
     }
-    
-    public function getActive(){
-        return( $this->active );
+
+    /**
+     * Get the value of active_code
+     */ 
+    public function getActive_code()
+    {
+        return $this->active_code;
     }
-    
-    public function setIdUserLevel( $id_user_level ){
-        $this->id_user_level = $id_user_level;
+
+    /**
+     * Set the value of active_code
+     *
+     * @return  self
+     */ 
+    public function setActive_code($active_code)
+    {
+        $this->active_code = $active_code;
+
+        return $this;
     }
-    
-    public function getIdUserLevel(){
-        return( $this->id_user_level );
+
+    /**
+     * Get the value of recovery_phrase
+     */ 
+    public function getRecovery_phrase()
+    {
+        return $this->recovery_phrase;
     }
-    
+
+    /**
+     * Set the value of recovery_phrase
+     *
+     * @return  self
+     */ 
+    public function setRecovery_phrase($recovery_phrase)
+    {
+        $this->recovery_phrase = $recovery_phrase;
+
+        return $this;
+    }
 }
 
 
