281f0a9dc32f660142734a70119f3d64513bcc8a
Ghriphos <luisfernando._alves@hotmail.com>
2023-10-07 18:54:44 -0300

    feat: only admin can create users

--- app/controllers/CtrlUsers.php
+++ app/controllers/CtrlUsers.php
@@ -7,37 +7,39 @@ $url = parse_url($_SERVER['REQUEST_URI']);
 $uriPath = strtolower($url['path']);
 
 if ($uriPath == '/user/add') {
-    if (strtolower($_SERVER['REQUEST_METHOD']) == 'post'){
-        $json = file_get_contents('php://input');
-
-        if ($json === false){
-            http_response_code(400);
-            echo json_encode(array('message' => 'Error receiving json'));
-        } else {
-            $data = json_decode($json, true);
-
-            if ($data === null) {
+    if (isset($_SESSION['idNivelUsuario']) && $_SESSION['idNivelUsuario'] == 1){
+        if (strtolower($_SERVER['REQUEST_METHOD']) == 'post'){
+            $json = file_get_contents('php://input');
+    
+            if ($json === false){
                 http_response_code(400);
-                echo json_encode(array('message' => 'Empty json'));
+                echo json_encode(array('message' => 'Error receiving json'));
             } else {
-                $idNivelUsuario  = $data['idNivelUsuario'];
-                $name            = $data['name'];
-                $email           = $data['email'];
-                $password        = $data['password'];
-                $active_code     = $data['active_code'];
-                $recovery_phrase = $data['recovery_phrase'];
-
-                $user = new User(0, $idNivelUsuario, $name, $email, $password, 0, $active_code, $recovery_phrase);
-
-                if ($user->save()) {
-                    echo json_encode(array('message' => '0'));
+                $data = json_decode($json, true);
+    
+                if ($data === null) {
+                    http_response_code(400);
+                    echo json_encode(array('message' => 'Empty json'));
                 } else {
-                    echo json_encode(array('message' => '1'));
-                }
-            }   
+                    $idNivelUsuario  = $data['idNivelUsuario'];
+                    $name            = $data['name'];
+                    $email           = $data['email'];
+                    $password        = $data['password'];
+                    $active_code     = $data['active_code'];
+                    $recovery_phrase = $data['recovery_phrase'];
+    
+                    $user = new User(0, $idNivelUsuario, $name, $email, $password, 0, $active_code, $recovery_phrase);
+    
+                    if ($user->save()) {
+                        echo json_encode(array('message' => '0'));
+                    } else {
+                        echo json_encode(array('message' => '1'));
+                    }
+                }   
+            }
+        } else {
+            echo json_encode(array('message' => 'Method not allowed'));
         }
-    } else {
-        echo json_encode(array('message' => 'Method not allowed'));
     }
 }
 
@@ -61,6 +63,9 @@ if ($uriPath == '/login') {
                 $where = new Where();
                 $where->addCondition('AND', 'email', '=', $email);
                 $where->addCondition('AND', 'password', '=', $password);
+
+                // uncomment line below here when email send work correctly to activate users
+
                 // $where->addCondition('AND', 'active', '=', 1);
 
                 $user = new User('','','','','','','','','');
