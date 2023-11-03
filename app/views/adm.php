5f5168b6f669b04cff8f5dbb141d6a00b640c965
ceciliascm <ceciliasoarescosta@gmail.com>
2023-11-02 23:44:49 -0300

    del: update forms + table advertisement

--- app/views/adm.php
+++ app/views/adm.php
@@ -206,67 +206,94 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
                             <input type="radio" name="radio-8" class="radio radio-error" />
                         </div>
 
-                <div class="w-12/12 mr-4 ml-4 my-4">
-                    <div class="avatar ml-96">
-                        <div class="w-24 rounded-full mt-4 -my-12 lg:shadow">
-                        <img src="/app/views/images/update-icon.png" class="w-16" />
-                        </div>
-                    </div> 
-                        <div class="p-6 shadow-lg bg-white rounded-lg">
-                        <h1 class="text-2xl font-semibold leading-relaxed text-gray-800">Atualizar</h1>
-                        <hr class="mt-4 w-full">
-                        <form action="" class="grid grid-cols-2 gap-4 mt-6 ml-24">
-                        <div class="mt-3">
-                                <label for="email" class="block text-base mb-2">Id</label>
-                                <input type="text" placeholder="Digite o Id.." class="input input-bordered w-full max-w-xs bg-gray-100" />
-                            </div>
-                            <div class="mt-3">
-                                <label for="email" class="block text-base mb-2">Nome</label>
-                                <input type="text" placeholder="Digite o nome.." class="input input-bordered w-full max-w-xs bg-gray-100" />
-                            </div>
-                            <div class="mt-3">
-                                <label for="email" class="block text-base mb-2">Descrição</label>
-                                <input type="text" placeholder="Digite a descrição.." class="input input-bordered w-full max-w-xs bg-gray-100" />
-                            </div>
-                            <div class="mt-3">
-                                <label for="email" class="block text-base mb-2">Preço</label>
-                                <input type="text" placeholder="Digite o preço.." class="input input-bordered w-full max-w-xs bg-gray-100" />
-                            </div>
-                            <div class="mt-3">
-                                <label for="email" class="block text-base mb-2">Id da Categoria</label>
-                                <input type="text" placeholder="Digite o Id.." class="input input-bordered w-full max-w-xs bg-gray-100" />
-                            </div>
-                            <div class="mt-3">
-                                <label for="email" class="block text-base mb-2">Peso</label>
-                                <input type="text" placeholder="Digite o peso.." class="input input-bordered w-full max-w-xs bg-gray-100" />
+                        <div class="overflow-x-auto my-4">
+                            <table class="table">
+                                <!-- head -->
+                                <thead>
+                                <tr>
+                                
+                                    <th></th>
+                                    <th>Nome</th>
+                                    <th>Descrição</th>
+                                    <th>Preço</th>
+                                    <th>Tamanho</th>
+                                    <th>Peso</th>
+                                    <th>Video Url</th>
+                                    <th>Foto (base64)</th>
+                                    <th class="flex justify-center">Action</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <!-- row 1 -->
+                                <tr>
+                                <tr class="hover">
+                                    <th>1</th>
+                                    <td>Conjunto Freio à disco</td>
+                                    <td>Conjunto de instalação de freio à disco para bicicletas. Contendo dois discos, duas pinças com quatro pastilhas, dois cabos com conduites</td>
+                                    <td>R$150</td>
+                                    <td>-</td>
+                                    <td>-</td>
+                                    <td>-</td>
+                                    <td>-</td>
+                                    <td class="flex justify-center">
+                                    <div class="badge badge-success gap-2 mr-3 flex justify-center">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-4 h-4 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+                                            success
+                                    </div>
+                                    <div class="badge badge-error gap-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-4 h-4 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+                                            error
+                                    </div>
+                                    </td>
+                                   
+                                </tr>
+                                <!-- row 2 -->
+                                <tr class="hover">
+                                    <th>2</th>
+                                    <td>Coroa</td>
+                                    <td>Coroa convencional para bicicletas sem marcha. 44 dentes</td>
+                                    <td>R$150</td>
+                                    <td>-</td>
+                                    <td>-</td>
+                                    <td>-</td>
+                                    <td>-</td>
+                                    <td class="flex justify-between">
+                                    <div class="badge badge-success gap-2 mr-3">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-4 h-4 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+                                            success
+                                    </div>
+                                    <div class="badge badge-error gap-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-4 h-4 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+                                            error
+                                    </div>
+                                    </td>
+                                </tr>
+                                <!-- row 3 -->
+                                <tr>
+                                <tr class="hover">
+                                    <th>3</th>
+                                    <td>Pedivela</td>
+                                    <td>Pedivila convencional de alumínio, 44mm</td>
+                                    <td>R$150</td>
+                                    <td>-</td>
+                                    <td>-</td>
+                                    <td>-</td>
+                                    <td>-</td>
+                                    <td class="flex justify-between">
+                                    <div class="badge badge-success gap-2 mr-3">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-4 h-4 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+                                            success
+                                    </div>
+                                    <div class="badge badge-error gap-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-4 h-4 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+                                            error
+                                    </div>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
                             </div>
 
-                            <div class="mt-3">
-                                <label for="email" class="block text-base mb-2">Tamanho</label>
-                                <input type="text" placeholder="Digite o tamanho.." class="input input-bordered w-full max-w-xs bg-gray-100" />
-                            </div>
-                            <div class="mt-3">
-                                <label for="email" class="block text-base mb-2">Url</label>
-                                <input type="text" placeholder="Digite a Url.." class="input input-bordered w-full max-w-xs bg-gray-100" />
-                            </div>
-
-                            <div class="mt-3">
-                                <label for="email" class="block text-base mb-2">Foto</label>
-                                <input type="file" class="file-input file-input-bordered file-input-bg-orange-500 w-full max-w-xs bg-gray-100" />
-                            </div>
-
-                            <div class="flex justify-center items-center mt-6 mr-24">
-                                <button class="btn btn-default bg-orange-500 hover:bg-black font-black border-orange-500 text-white">Submit</button>
-                            </div>
-                        </form>
-
-                        <div class="hidden">
-                        <div class="alert alert-success">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                                    <span>Seu anúncio foi inserido!</span>
-                                </div>
-                        </div>
-                        </div>
 
                         
               </main>
