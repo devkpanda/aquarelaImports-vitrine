a6f3ba416eb03a5787545034b179dd49667400cd
Ghriphos <luisfernando._alves@hotmail.com>
2023-11-15 21:00:27 -0300

    feat: /category/listall route

--- app/controllers/CtrlCategory.php
+++ app/controllers/CtrlCategory.php
@@ -20,31 +20,59 @@ $uriPath = $url['path'];
 
 if ($uriPath == '/category/add'){
     if(strtolower($_SERVER['REQUEST_METHOD']) == 'post') {
-        // n entendi direito isso aq
         $json = file_get_contents('php://input');
+
         if ($json === false){
             http_response_code(400);
-            echo json_encode(array('message' => 'Error receiving json'));
+            die(json_encode(array('message' => 'Error receiving json')));
         } else {
             $data = json_decode($json, true);
+
             if ($data === null){
                 http_response_code(400);
                 echo json_encode(array('message' => 'Empty json'));
             } else {
-                $description = $data['description'];
-                $parent_id   = $data['parent_id'];
-
-                $category = new Category(0, $description, $parent_id );
-            
-                if ($category->save()) {
-                    echo json_encode(array('message' => '0'));
+                if (!isset($data['description']) || !isset($data['parent_id'])) {
+                    die(json_encode(array('message' => 'unexpected JSON')));
                 } else {
-                    echo json_encode(array('message' => '1'));
+                    $description = $data['description'];
+                    $parent_id   = $data['parent_id'];
+    
+                    $category = new Category(0, $description, $parent_id );
+                
+                    if ($category->save()) {
+                        echo json_encode(array('message' => '0'));
+                    } else {
+                        echo json_encode(array('message' => '1'));
+                    }
                 }
             }
         }
-
     } else {
         echo json_encode(array('message' => 'Method not allowed'));
+    }
+}
+
+if ($uriPath == '/category/listall') {
+    if (strtolower($_SERVER['REQUEST_METHOD']) == 'get'){
+        $category = new Category(0, '', '');
+
+        $categories = $category->listCategory();
+
+        $json = array();
+
+        foreach ($categories as $category) {
+            $categoryArray = array(
+                "id" => $category->getId(),
+                "description" => $category->getDescription(),
+                "parent_id" => $category->getParent_id()
+            );
+
+            $json[] = $categoryArray;
+        }
+
+        die(json_encode($json));
+    } else {
+        die(json_encode(array('message' => 'Method not allowed')));
     }
 }
