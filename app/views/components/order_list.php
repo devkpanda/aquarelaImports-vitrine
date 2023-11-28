65ccc6c2a83c3e13dded808c7ae84e209f63cc2c
ceciliascm <ceciliasoarescosta@gmail.com>
2023-11-28 00:17:37 -0300

    order_list on view

---
+++ app/views/components/order_list.php
@@ -0,0 +1,18 @@
+<div class="hidden w-full mt-10 px-8" id="category_search">
+    <div class="max-w-7xl mx-auto shadow-xl gap-10 bg-white rounded-[10px] p-6">
+        <div class="flex flex-col overflow-auto">
+            <div class="my-4 ">
+                <table class="table">
+                    <!-- head -->
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Id do anúncio</th>
+                        </tr>
+                    </thead>
+                    <tbody id="order_listing"></tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
