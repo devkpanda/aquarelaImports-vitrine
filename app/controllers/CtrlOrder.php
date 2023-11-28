8d53eb7dff4c2718949480929cf69300ce78c079
ceciliascm <ceciliasoarescosta@gmail.com>
2023-11-27 23:24:31 -0300

    order delete

--- app/controllers/CtrlOrder.php
+++ app/controllers/CtrlOrder.php
@@ -67,14 +67,57 @@ if ($uriPath == '/order/listall'){
 
             $json[] = $orderArray;
 
-        }
+        
         http_response_code(200);
         die(json_encode($json));
     } else {
         die(json_encode(array('message' => 'Method not allowed')));
     }
+}
 
 
+
+    if ($uriPath == '/order/delete') {
+        if (strtolower($_SERVER['REQUEST_METHOD']) == 'post') {
+            $json = file_get_contents('php://input');
+    
+            if ($json === false) {
+                http_response_code(400);
+                echo json_encode(array('message' => 'Error receiving json'));
+            } else {
+                $data = json_decode($json, true);
+    
+                if ($data === null) {
+                    http_response_code(400);
+                    echo json_encode(array('message' => 'Empty json'));
+                } else {
+                        try {
+                            $id = $data['id'];
+    
+                            $order = new Order($id, '');
+                            $order->setId($id);
+    
+                            if (!$order->delete()->rowCount() == 1) {
+                                http_response_code(400);
+                                die(json_encode(array('message' => 'Houve um erro ao excluir o pedido')));
+                            } else {
+                                die(json_encode(array('message' => 'Pedido excluido com sucesso')));
+                            }
+                        } catch (Exception $e) {
+                            http_response_code(400);
+                            die(json_encode(array('message' => $e->getMessage())));
+                        } catch (InvalidArgumentException $iae) {
+                            http_response_code(400);
+                            die(json_encode(array('message' => $iae->getMessage())));
+                        }
+                    }
+                }
+            }
+        } else {
+            http_response_code(400);
+            die(json_encode(array('message' => 'sem permissao parça'))); 
+        }
+    
 
 
 
